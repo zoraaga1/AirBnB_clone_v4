15c074030763449e9b1fc05f80a930ac6b9b6331
zoraaga1 <fati.sousse12@gmail.com>
2024-04-06 14:27:35 +0000

    refac

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    let amenityIDs = {};
+$(document).ready(function () {
+  const amenityIDs = {};
 
-    $('input[type="checkbox"]').change(function() {
-        let amenityID = $(this).attr('data-id');
-        if ($(this).is(':checked')) {
-            amenityIDs[amenityID] = true;
-        } else {
-            delete amenityIDs[amenityID];
-        }
+  $('input[type="checkbox"]').change(function () {
+    const amenityID = $(this).attr('data-id');
+    if ($(this).is(':checked')) {
+      amenityIDs[amenityID] = true;
+    } else {
+      delete amenityIDs[amenityID];
+    }
 
-        let amenitiesList = Object.keys(amenityIDs).join(', ');
-        $('.amenities h4').text('Amenities: ' + amenitiesList);
-    });
+    const amenitiesList = Object.keys(amenityIDs).join(', ');
+    $('.amenities h4').text('Amenities: ' + amenitiesList);
+  });
 });
